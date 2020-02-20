144c44a2f71e10dcee0150adbad68ecc00a079e7
Peyton Casper <peytoncas@gmail.com>
2020-02-19 22:30:03 -0500

    Bumped module versions to 1.0.2

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 module "aws_instance" {
     source  = "app.terraform.io/service-now-test/module-example/aws"
-    version = "1.0.1"
+    version = "1.0.2"
     instance_type = "t2.medium"
 
     instance_count = var.cloud == "aws" ? 1 : 0
@@ -14,7 +14,7 @@ module "aws_instance" {
 
 module "azure_instance" {
     source  = "app.terraform.io/service-now-test/module-example/azure"
-    version = "1.0.1"
+    version = "1.0.2"
     instance_type = "Standard_DS1_v2"
 
     instance_count = var.cloud == "azure" ? 1 : 0
