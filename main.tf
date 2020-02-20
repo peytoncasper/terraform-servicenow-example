f025039efbceebdd55e7d65094bb31f1396894d6
Peyton Casper <peytoncas@gmail.com>
2020-02-19 21:31:50 -0500

    Migrated AWS Module to separate repo and added reference

--- main.tf
+++ main.tf
@@ -4,3 +4,8 @@ provider "aws" {
 
 # provider "azurerm" {}
 
+module "aws_instance" {
+    source  = "app.terraform.io/service-now-test/module-example/aws"
+    version = "1.0.0"
+    instance_type = "t2.medium"
+}
